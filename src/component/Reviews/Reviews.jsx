6b67ee880c81e856e3c481cf291b65ab02821a49
Alexanderadon <fistin_103@mail.ru>
2023-06-20 19:05:51 +0600

    Исправил страницу сделал поиск по странам

--- src/component/Reviews/Reviews.jsx
+++ src/component/Reviews/Reviews.jsx
@@ -13,8 +13,8 @@ function Reviews() {
         733 отзывов оставили клиенты за последние 12 месяцев. Из них 1 598 —
         положительные.
       </p>
-      {/* block reviews */}
       <div className="reviews__block">
+        {/* block reviews */}
         <div className="reviews__item">
           <div className="reviews__users">
             <div className="reviews__avatar">
@@ -49,7 +49,289 @@ function Reviews() {
             </a>
           </div>
         </div>
+         {/* block reviews */}
+         <div className="reviews__item">
+          <div className="reviews__users">
+            <div className="reviews__avatar">
+              <img className="reviews__img" src={Masters} alt="avatar" />
+            </div>
+            <div className="reviews__info">
+              <p className="reviews__name">Эмель Айкач</p>
+              <span className="reviews__state">
+                <span>
+                  <img className="revoews__state-star" src={Star} alt="" />
+                  4.9
+                </span>
+                <span>236 отзывов</span>
+                <span>Очень хвалят</span>
+              </span>
+            </div>
+          </div>
+          <div className="reviews__content">
+            <div className="reviews__content-title">
+              Юлия оставила отзыв
+              <div className="reviews__content-stars">
+                <img src={BgStar} alt="star" />
+                <img src={BgStar} alt="star" />
+                <img src={BgStar} alt="star" />
+                <img src={BgStar} alt="star" />
+                <img src={BgStar} alt="star" />
+              </div>
+            </div>
+            <p className="reviews__content-subtitle">Всё отлично! Спасибо!</p>
+            <a href="/" className="reviews__content-translate">
+              Перевод
+            </a>
+          </div>
+        </div>
+         {/* block reviews */}
+         <div className="reviews__item">
+          <div className="reviews__users">
+            <div className="reviews__avatar">
+              <img className="reviews__img" src={Masters} alt="avatar" />
+            </div>
+            <div className="reviews__info">
+              <p className="reviews__name">Эмель Айкач</p>
+              <span className="reviews__state">
+                <span>
+                  <img className="revoews__state-star" src={Star} alt="" />
+                  4.9
+                </span>
+                <span>236 отзывов</span>
+                <span>Очень хвалят</span>
+              </span>
+            </div>
+          </div>
+          <div className="reviews__content">
+            <div className="reviews__content-title">
+              Юлия оставила отзыв
+              <div className="reviews__content-stars">
+                <img src={BgStar} alt="star" />
+                <img src={BgStar} alt="star" />
+                <img src={BgStar} alt="star" />
+                <img src={BgStar} alt="star" />
+                <img src={BgStar} alt="star" />
+              </div>
+            </div>
+            <p className="reviews__content-subtitle">Всё отлично! Спасибо!</p>
+            <a href="/" className="reviews__content-translate">
+              Перевод
+            </a>
+          </div>
+        </div>
+         {/* block reviews */}
+         <div className="reviews__item">
+          <div className="reviews__users">
+            <div className="reviews__avatar">
+              <img className="reviews__img" src={Masters} alt="avatar" />
+            </div>
+            <div className="reviews__info">
+              <p className="reviews__name">Эмель Айкач</p>
+              <span className="reviews__state">
+                <span>
+                  <img className="revoews__state-star" src={Star} alt="" />
+                  4.9
+                </span>
+                <span>236 отзывов</span>
+                <span>Очень хвалят</span>
+              </span>
+            </div>
+          </div>
+          <div className="reviews__content">
+            <div className="reviews__content-title">
+              Юлия оставила отзыв
+              <div className="reviews__content-stars">
+                <img src={BgStar} alt="star" />
+                <img src={BgStar} alt="star" />
+                <img src={BgStar} alt="star" />
+                <img src={BgStar} alt="star" />
+                <img src={BgStar} alt="star" />
+              </div>
+            </div>
+            <p className="reviews__content-subtitle">Всё отлично! Спасибо!</p>
+            <a href="/" className="reviews__content-translate">
+              Перевод
+            </a>
+          </div>
+        </div>
+         {/* block reviews */}
+         <div className="reviews__item">
+          <div className="reviews__users">
+            <div className="reviews__avatar">
+              <img className="reviews__img" src={Masters} alt="avatar" />
+            </div>
+            <div className="reviews__info">
+              <p className="reviews__name">Эмель Айкач</p>
+              <span className="reviews__state">
+                <span>
+                  <img className="revoews__state-star" src={Star} alt="" />
+                  4.9
+                </span>
+                <span>236 отзывов</span>
+                <span>Очень хвалят</span>
+              </span>
+            </div>
+          </div>
+          <div className="reviews__content">
+            <div className="reviews__content-title">
+              Юлия оставила отзыв
+              <div className="reviews__content-stars">
+                <img src={BgStar} alt="star" />
+                <img src={BgStar} alt="star" />
+                <img src={BgStar} alt="star" />
+                <img src={BgStar} alt="star" />
+                <img src={BgStar} alt="star" />
+              </div>
+            </div>
+            <p className="reviews__content-subtitle">Всё отлично! Спасибо!</p>
+            <a href="/" className="reviews__content-translate">
+              Перевод
+            </a>
+          </div>
+        </div>
+         {/* block reviews */}
+         <div className="reviews__item">
+          <div className="reviews__users">
+            <div className="reviews__avatar">
+              <img className="reviews__img" src={Masters} alt="avatar" />
+            </div>
+            <div className="reviews__info">
+              <p className="reviews__name">Эмель Айкач</p>
+              <span className="reviews__state">
+                <span>
+                  <img className="revoews__state-star" src={Star} alt="" />
+                  4.9
+                </span>
+                <span>236 отзывов</span>
+                <span>Очень хвалят</span>
+              </span>
+            </div>
+          </div>
+          <div className="reviews__content">
+            <div className="reviews__content-title">
+              Юлия оставила отзыв
+              <div className="reviews__content-stars">
+                <img src={BgStar} alt="star" />
+                <img src={BgStar} alt="star" />
+                <img src={BgStar} alt="star" />
+                <img src={BgStar} alt="star" />
+                <img src={BgStar} alt="star" />
+              </div>
+            </div>
+            <p className="reviews__content-subtitle">Всё отлично! Спасибо!</p>
+            <a href="/" className="reviews__content-translate">
+              Перевод
+            </a>
+          </div>
+        </div>
+         {/* block reviews */}
+         <div className="reviews__item">
+          <div className="reviews__users">
+            <div className="reviews__avatar">
+              <img className="reviews__img" src={Masters} alt="avatar" />
+            </div>
+            <div className="reviews__info">
+              <p className="reviews__name">Эмель Айкач</p>
+              <span className="reviews__state">
+                <span>
+                  <img className="revoews__state-star" src={Star} alt="" />
+                  4.9
+                </span>
+                <span>236 отзывов</span>
+                <span>Очень хвалят</span>
+              </span>
+            </div>
+          </div>
+          <div className="reviews__content">
+            <div className="reviews__content-title">
+              Юлия оставила отзыв
+              <div className="reviews__content-stars">
+                <img src={BgStar} alt="star" />
+                <img src={BgStar} alt="star" />
+                <img src={BgStar} alt="star" />
+                <img src={BgStar} alt="star" />
+                <img src={BgStar} alt="star" />
+              </div>
+            </div>
+            <p className="reviews__content-subtitle">Всё отлично! Спасибо!</p>
+            <a href="/" className="reviews__content-translate">
+              Перевод
+            </a>
+          </div>
+        </div>
+         {/* block reviews */}
+         <div className="reviews__item">
+          <div className="reviews__users">
+            <div className="reviews__avatar">
+              <img className="reviews__img" src={Masters} alt="avatar" />
+            </div>
+            <div className="reviews__info">
+              <p className="reviews__name">Эмель Айкач</p>
+              <span className="reviews__state">
+                <span>
+                  <img className="revoews__state-star" src={Star} alt="" />
+                  4.9
+                </span>
+                <span>236 отзывов</span>
+                <span>Очень хвалят</span>
+              </span>
+            </div>
+          </div>
+          <div className="reviews__content">
+            <div className="reviews__content-title">
+              Юлия оставила отзыв
+              <div className="reviews__content-stars">
+                <img src={BgStar} alt="star" />
+                <img src={BgStar} alt="star" />
+                <img src={BgStar} alt="star" />
+                <img src={BgStar} alt="star" />
+                <img src={BgStar} alt="star" />
+              </div>
+            </div>
+            <p className="reviews__content-subtitle">Всё отлично! Спасибо!</p>
+            <a href="/" className="reviews__content-translate">
+              Перевод
+            </a>
+          </div>
+        </div>
+         {/* block reviews */}
+         <div className="reviews__item">
+          <div className="reviews__users">
+            <div className="reviews__avatar">
+              <img className="reviews__img" src={Masters} alt="avatar" />
+            </div>
+            <div className="reviews__info">
+              <p className="reviews__name">Эмель Айкач</p>
+              <span className="reviews__state">
+                <span>
+                  <img className="revoews__state-star" src={Star} alt="" />
+                  4.9
+                </span>
+                <span>236 отзывов</span>
+                <span>Очень хвалят</span>
+              </span>
+            </div>
+          </div>
+          <div className="reviews__content">
+            <div className="reviews__content-title">
+              Юлия оставила отзыв
+              <div className="reviews__content-stars">
+                <img src={BgStar} alt="star" />
+                <img src={BgStar} alt="star" />
+                <img src={BgStar} alt="star" />
+                <img src={BgStar} alt="star" />
+                <img src={BgStar} alt="star" />
+              </div>
+            </div>
+            <p className="reviews__content-subtitle">Всё отлично! Спасибо!</p>
+            <a href="/" className="reviews__content-translate">
+              Перевод
+            </a>
+          </div>
+        </div>  
       </div>
+
+
 
     </section>
   );
