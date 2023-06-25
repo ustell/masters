d5bb63610c9f8d89c1e81b01e24c62dcd032afd5
Димаш Ертлеуов <adamdimert@gmail.com>
2023-06-25 17:27:29 +0000

    Revert "Merge branch 'dimash' into 'main'"
    This reverts merge request !24

--- src/redux/services/axios.js
+++ src/redux/services/axios.js
@@ -1,12 +1,11 @@
 /** @format */
 
 import axios from "axios";
-
+// для сокращениея запросов axios
 const instance = axios.create({
   baseURL: "https://b11e-37-99-47-159.ngrok-free.app",
-  mode: "no-cors", // Add the 'no-cors' mode here
 });
-
+// middleware для проверки авторизации
 instance.interceptors.request.use((config) => {
   config.headers.Authorization =
     window.localStorage.getItem("token");
