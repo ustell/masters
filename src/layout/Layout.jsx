906041ed9db5ea18eddad95a071783ea10a53ce7
Adamdim <101868494+AdamDimErt@users.noreply.github.com>
2023-06-27 23:05:56 +0600

    update

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -3,13 +3,21 @@
 import React from "react";
 import Header from "../component/Header/Header";
 import { Footer } from "../component/Footer/Footer";
+import { Path } from "../path";
 
 export const Layout = ({ children }) => {
+  const currentPath = window.location.pathname;
+
+  let footerComponent = null;
+  if (currentPath === "/" || currentPath === "/home") {
+    footerComponent = <Footer />;
+  }
+
   return (
     <>
       <Header />
       {children}
-      <Footer />
+      {footerComponent}
     </>
   );
 };
